85b78bb24b59a5d2e7f0adaa7aeedb8238283a99
PC86\Alumno <avileslupion.david6@gmail.com>
2022-10-10 20:11:20 +0200

    Starting Dialogue

---
+++ Assets/Scripts/System Storie/BoolCheck.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class BoolCheck
+{
+    //Progress.
+
+    public static bool phase1;
+    public static bool phase2;
+    public static bool phase3;
+
+
+}
---
+++ Assets/Scripts/System Storie/NPCs.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "NPC", menuName = "ScriptableObjects/NPC", order = 1)]
+public class NPCs : ScriptableObject
+{
+
+    public string Name;
+
+    public Sprite[] Faces;
+
+    
+
+
+}
